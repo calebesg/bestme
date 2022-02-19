32d4956092b74db359fcb472692923312e3944d2
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-19 14:58:18 -0300

    bestme

--- index.js
+++ index.js
@@ -19,7 +19,25 @@ process.stdin.on('data', data => {
   if (answers.length < questions.length) {
     ask(answers.length);
   } else {
-    
     process.exit();
   }
+});
+
+process.on('exit', () => {
+  console.log(`
+    Parabéns Calebe
+
+    Hoje você aprendeu:
+    ${answers[0]}
+
+    O que te deixou aborrecido, e o que fazer a respeito:
+    ${answers[1]}
+
+    Hoje você ficou feliz por:
+    ${answers[2]}
+
+    Hoje você ajudou ${answers[3]} pessoa(s).
+
+    Muito bom, volte amanhã para mais reflexões.
+  `);
 });
