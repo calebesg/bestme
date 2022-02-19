b93a4bea014ef1c39323b28e39a8278a31ab532c
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-19 14:25:01 -0300

    created a array of questions

---
+++ index.js
@@ -0,0 +1,10 @@
+const questions = [
+  'O que aprendi hoje?',
+  'O que me deixou aborrecido? O que poderia fazer para melhorar?',
+  'O que me deixou feliz hoje?',
+  'Quantas pessoas você ajudou hoje?',
+];
+
+const ask = (index = 0) => {
+
+}
