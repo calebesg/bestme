7f83978f27af7597d43c226d36ec25e4900740fa
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-19 14:38:25 -0300

    create answer array

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ const questions = [
 ];
 
 const ask = (index = 0) => {
-
+  process.stdout.write("\n" + questions[index] + ": ");
 }
+
+ask();
+
+const answers = [];
+
+process.stdin.on('data', data => {
+  answers.push(data.toString().trim());
+
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    
+    process.exit();
+  }
+});
